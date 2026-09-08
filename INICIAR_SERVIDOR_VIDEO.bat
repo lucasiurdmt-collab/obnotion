a5a3c1e6c0416b801d8d2ce7803abb6ed459a438
lucasiurdmt-collab <lucasiurdmt@gmail.com>
2026-09-08 20:28:58 -0300

    feat: Adicionado mini-servidor local de videos para funcionar no vMix e tablet

---
+++ INICIAR_SERVIDOR_VIDEO.bat
@@ -0,0 +1,12 @@
+@echo off
+title Servidor de Videos - Obnotion
+echo.
+echo   =============================================
+echo        SERVIDOR DE VIDEOS - OBNOTION
+echo   =============================================
+echo.
+echo   Iniciando servidor...
+echo.
+node "%~dp0servidor_video.js"
+echo.
+pause
